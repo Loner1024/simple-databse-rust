12f01be5407e7171d8fad6e4bc723a1dacb5478d
Loner1024 <xth12138@gmail.com>
2023-03-04 00:52:16 +0800

    feat:simplest SQL compiler and Virtual Machine

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use std::io::{stdin, stdout, Write};
-use std::process::{ExitCode};
-
-fn print_prompt() {
-    print!("db > ");
-    stdout().flush().expect("failed out to std");
-}
+use std::process::{exit, ExitCode};
 
 fn main() -> ExitCode {
     loop {
@@ -14,9 +9,74 @@ fn main() -> ExitCode {
             Ok(_) => {}
             Err(err) => panic!("failed to read line: {}", err)
         }
-        if buf.trim().eq(".exit") {
-            return ExitCode::SUCCESS;
+        buf = buf.trim().to_string();
+        if buf.starts_with('.') {
+            match do_meta_command(&buf) {
+                Ok(_) => continue,
+                Err(_) => {
+                    println!("Unrecognized command '{}'", buf);
+                    continue;
+                }
+            }
         }
-        println!("Unrecognized command {}", buf);
+        match prepare_statement(&buf) {
+            Ok(statement) => {
+                execute_statement(&statement);
+                println!("Executed.");
+                continue;
+            }
+            Err(_) => {
+                println!("Unrecognized keyword at start of '{}'", buf);
+                continue;
+            }
+        }
     }
 }
+
+fn print_prompt() {
+    print!("db > ");
+    stdout().flush().expect("failed out to std");
+}
+
+struct MetaCommandErr;
+
+
+fn do_meta_command(command: &String) -> Result<(), MetaCommandErr> {
+    if command.eq(".exit") {
+        println!("Bye!");
+        exit(0);
+    }
+    Err(MetaCommandErr)
+}
+
+struct PrepareErr;
+
+enum StatementType {
+    Insert,
+    Select,
+}
+
+struct Statement {
+    statement_type: StatementType,
+}
+
+fn prepare_statement(command: &String) -> Result<Statement, PrepareErr> {
+    match command {
+        c if c.starts_with("insert") => {
+            Ok(Statement { statement_type: StatementType::Insert })
+        }
+        c if c.starts_with("select") => {
+            Ok(Statement { statement_type: StatementType::Select })
+        }
+        _ => Err(PrepareErr)
+    }
+}
+
+fn execute_statement(statement: &Statement) {
+    match statement.statement_type {
+        StatementType::Insert => println!("This is where we would do an insert."),
+        StatementType::Select => println!("This is where we would do an select.")
+    }
+}
+
+
